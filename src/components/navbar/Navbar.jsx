9501b7c80354e019ff223476d68ac8cb60c1beb8
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-27 10:42:00 -0800

    'Navbar'

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import './Navbar.css'
 import {close ,logo,menu} from '../../assets'
 function Navbar() {
+  const [toggle, setToggle] = useState(false)
   return (
-    <div>
-      <img src={logo} alt="HooBank" />
+    <div className='navbar'>
+      <div className="navbar_logo">
+        <img src={logo} alt="HooBank" />
+      </div>
+      <div className="navbar_list">
+        <ul>
+          <li>Home</li>
+          <li>Features</li>
+          <li>Product</li>
+          <li>Clients</li>
+        </ul>
+      </div>
+  {/* <div className=" navbar_sm navbar_hidden nav-flex">
+    <img src={toggle? close :menu} alt="menu" 
+    onClick={()=>setToggle((prev)=>!prev)}
+    />
+    <div className={`${toggle ?  "nav-flex" : "navbar_hidden"}`} id="menu" ></div>
+  </div> */}
     </div>
   )
 }
