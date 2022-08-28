65abcfff219305c46dab1065ac077e11d23ae54f
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-28 10:10:21 -0800

    'Stats'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Stats from './components/Stats/Stats'
 import Hero from  './components/Hero/Hero'
 function App() {
   return (
     <div className="App">
      <Navbar/>
       <Hero/>
+      <Stats/>
     
 
 
--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import './Stats.css'
 const Stats = () => {
   return (
-    <div>
-      
+    <div className='Stats'>
+      <div className="Stats_content">
+        <div className="Stats_content-state">
+        <h2>3800+</h2>
+       <h4>User Active</h4>
+        </div>
+        <div className="Stats_content-state">
+        <h2>$230+</h2>
+       <h4>TRUSTED BY COMPANY</h4>
+        </div>   
+        <div className="Stats_content-state">
+        <h2>$230M+</h2>
+       <h4>TRANSACTION</h4>
+        </div>   
+      </div>
     </div>
   )
 }
