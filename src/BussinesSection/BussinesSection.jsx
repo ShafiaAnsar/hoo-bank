2c91fb55b0b3604b433fc3cd2c245548d67950b6
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-29 02:11:43 -0800

    'BussinesSection'

--- src/BussinesSection/BussinesSection.jsx
+++ src/BussinesSection/BussinesSection.jsx
@@ -9,40 +9,43 @@ const BussinesSection = () => {
         <div className="bsection_content">
         <div className="bsection_content-left">
             <h2>
-            You do the business, we’ll handle the money.
+            You do the business, <br/>we’ll handle the money.
             </h2>
             <p>With the right credit card, you can improve your financial life by<br/> building credit, earning rewards and saving money.<br/> But with hundreds of credit cards on the market.</p>
+            <div className="left-button">
+              <button>Get Started</button>
+            </div>
         </div>
         <div className="bsection_content-right">
-            <div className="rewards">
-                <div className="rewards_icon">
-                <StarsIcon/>
-                </div>
-                <div className="reward_text">
-                <h5>Rewards</h5>   
-                <p>We take proactive steps make sure your information<br/> and transactions are secure.</p>
-                </div>
-                <div className="rewards">
-                <div className="rewards_icon">
-                <OfflinePinIcon/>
-                </div>
-                <div className="reward_text">
-                <h5>100% Secured</h5>   
-                <p>We take proactive steps make sure your information <br/>and transactions are secure.</p>
+          <div className="rewards">
+            <div className="rewards_heading">
+                <StarsIcon className='rewards_icon'/>
+                <h5>Rewards</h5>  
             </div>
-            </div>
-            </div>
-            <div className="rewards">
-                <div className="rewards_icon">
-                <SendIcon/>
-                </div>
-                <div className="reward_text">
-                <h5>100% Secured</h5>   
-                <p>We take proactive steps make sure your information <br/>and transactions are secure.</p>
-            </div>
-            </div>
+            <div className="reward_text">
+             <p>The best credit cards offer some tantalizing<br/> combinations of promotions and prizes</p>
+             </div>
           </div>
+          <div className="rewards">
+            <div className="rewards_heading">
+                <OfflinePinIcon className='rewards_icon'/>
+                <h5>100% Secured</h5>   
+            </div>
+            <div className="reward_text">
+            <p>We take proactive steps make sure your<br/> information are secure.</p>
+             </div>
+          </div><div className="rewards">
+            <div className="rewards_heading">
+                <SendIcon className='rewards_icon' id='transfer'/>
+                <h5>Balance Transfer</h5> 
+            </div>
+            <div className="reward_text" >
+            <p>A balance transfer credit card can save you <br/> a lot of money in interest charges.</p>
+             </div>
           </div>
+            
+      </div>
+        </div>
      
     </div>
   )
