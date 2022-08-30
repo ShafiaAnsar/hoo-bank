119a267bb42d412df860094ab0bbb3ed8e848817
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-30 10:40:26 -0800

    'Stats responsive'

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
      {/* <Navbar/> */}
       <Hero/>
-      {/* <Stats/> */}
+      <Stats/>
       {/* <BussinesSection/>
       <Billing/>
       <CardDeal/>
