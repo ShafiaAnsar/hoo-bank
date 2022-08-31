6e09fc7b6f5afc22adef2538fb4b39f799d20b35
Shafia Ansar <shafiaansar142@gmail.com>
2022-08-30 23:34:39 -0800

    'Testimonials'

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ function App() {
       <BussinesSection/>
       <Billing/>
       <CardDeal/>
-      {/*<Testimonials/>
-      <CTA/>
+     <Testimonials/>
+       {/*<CTA/>
       <Footer/> */}
 
 
